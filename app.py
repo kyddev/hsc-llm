7f785a17075ee73f688fab467ed6cfad2a41ef8b
kyddev <123540720+kyddev@users.noreply.github.com>
2024-11-28 14:17:19 +1100

    Updated app
    Can create vector database without any errors
    
    removed keyword extractor

--- app.py
+++ app.py
@@ -1,180 +1,152 @@
-#Import for User Interface
+import os 
+import tempfile
+
+import chromadb
+import ollama
 import streamlit as st
 
-#Import for model and responses
-from langchain_core.messages import HumanMessage, AIMessage
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.vectorstores import Chroma
+from pypdf import PdfReader
+import ollama
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import CrossEncoder
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 from langchain_community.chat_models import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_core.output_parsers import StrOutputParser
 
-#Import for PDF text extraction and splitting
-from langchain_community.document_loaders import PyMuPDFLoader
-from pypdf import PdfReader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-#Import for vector database and embeddings
-import os
-from langchain_community.embeddings.ollama import OllamaEmbeddings
-from langchain_chroma import Chroma
-from uuid import uuid4
-from langchain_core.documents import Document
-
-#Import for keyword extraction
-import yake
-
-#Import for relative current working directory
-import os
-import sys
-
-###########################
-
-
-#Initialising UI
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
 st.set_page_config(page_title="Study AI")
 
-st.title("Study AI")
-
-#Initialising LLM 
-llm = ChatOllama(model="llama3")
-
-#Initialising the Embedding model
-embedding_model = OllamaEmbeddings(model='nomic-embed-text')
-
-#Initialising the keyword extraction model
-kw_extractor = yake.KeywordExtractor()
-language = "en"
-
-#Initialising Current Working Directory
-app_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
+st.title("Study AI")  
 
 
-#Rewrite-Retrieve-Read Implementation
-def rewrite_query(query: str):
 
-    query_rewriting_str = """
-    You are an expert at reformulating questions. \
-    Your reformulated questions are understandable for high students and teachers.
-    The question you reformulate will begin and end with with ’**’. 
-    
-    Question: 
-    {question} 
+system_prompt = """
+You are an AI assistant tasked with providing detailed answers based solely on the given context. Your goal is to analyze the information provided and formulate a comprehensive, well-structured response to the question.
 
-    Only reply using a complete sentence and only give the answer in the following format:
-    **Question**"""
+context will be passed as "Context:"
+user question will be passed as "Question:"
 
-    query_rewriting_prompt = ChatPromptTemplate.from_template(query_rewriting_str)
+To answer the question:
+1. Thoroughly analyze the context, identifying key information relevant to the question.
+2. Organize your thoughts and plan your response to ensure a logical flow of information.
+3. Formulate a detailed answer that directly addresses the question, using only the information provided in the context.
+4. Ensure your answer is comprehensive, covering all relevant aspects found in the context.
+5. If the context doesn't contain sufficient information to fully answer the question, state this clearly in your response.
 
-    chain = query_rewriting_prompt | llm | StrOutputParser()
+Format your response as follows:
+1. Use clear, concise language.
+2. Organize your answer into paragraphs for readability.
+3. Use bullet points or numbered lists where appropriate to break down complex information.
+4. If relevant, include any headings or subheadings to structure your response.
+5. Ensure proper grammar, punctuation, and spelling throughout your answer.
 
-    response = chain.invoke({
-        "question" : query
-    })
+IMPORTANT:L Do not mention references, sources, or citations in your response. There is no need to reference the context in your response.
+If the documents provided are not relevant to the question, use your own knowledge to answer.
 
-    print(response)
+"""
 
-    return response 
+def process_document(file_uploaded: UploadedFile) -> list[Document]:
+    # Store uploaded file as a temp file
+    temp_file = tempfile.NamedTemporaryFile("wb", suffix=".pdf", delete=False)
+    temp_file.write(file_uploaded.read())
 
-def get_keywords(rewritten_query):
-    keywords = kw_extractor.extract_keywords(rewritten_query)
-    for kw in keywords:
-        print(kw)
-    return keywords
+    docs = ""
+    pdf = PdfReader(temp_file.name)
+    for page in pdf.pages:
+        docs += page.extract_text()
 
-def get_response(query, context):
-    qa_template = """
-    You are an assistant for question-answering tasks.
-
-    Use the following documents that is retrieved from the database is relevant \
-    use it to provide a complete and concise response to the user's query. \
-    Do not mention references, sources, or citations in your response
-
-    If the documents provided are not relevant to the question, use your own knowledge to answer.
-
-    Limit your answer to 3-4 sentences.
-
-    User question: {user_questions}
-
-    Documents: {documents}
-
-    Answer:
-    """
-
-    qa_prompt = ChatPromptTemplate.from_template(qa_template)
-
-    llm = ChatOllama(model="llama3")
-
-    chain = qa_prompt | llm | StrOutputParser()
-
-    return chain.invoke({
-        "user_questions" : query,
-        "documents" : context
-    })
-
-#Function that extracts the text form the PDFs uploaded
-def get_pdf_text(pdf):
-    text=""
-    count = 0
-    for pdf in pdf:
-        count += 1
-        print(count)
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
-
-#Function that splits the text into chunks for the embedding function
-def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
+        chunk_size=500,
         chunk_overlap=100,
         length_function=len,
         is_separator_regex=False,    
     )
-    chunks = text_splitter.split_text(text)
-    return chunks
 
-#Function that loads or creates a vector store
-def create_or_load_vector_store(embeddings, store_name):
-    persistent_directory = os.path.join(app_dir, store_name)
-    if not os.path.exists(persistent_directory):
-        print("creating vector store")
-        vector_store = Chroma.from_documents(embeddings, persist_directory=persistent_directory)
-    else:
-        print("loading existing vector store")
-        vector_store = Chroma(
-        persist_directory=persistent_directory,
-        embedding_function=embeddings
-        )
-    return vector_store
+    return text_splitter.split_text(docs)
 
-vector_store = create_or_load_vector_store(embedding_model, "chroma_db")
+def get_collection():
 
-#Function that adds the documents to the vector store
-def add_new_docs(pdf):
-    text = get_pdf_text(pdf)
-    chunks = get_text_chunks(text)
-    documents = [Document(page_context=text) for text in chunks]
-    uuids = [str(uuid4()) for _ in range(len(chunks))]
-    vector_store.add_documents(documents=documents, ids=uuids)
+    embedding_model = OllamaEmbeddings(model="nomic-embed-text:latest")
 
-#Function that retrieves the most relevant data from vector database
-def perform_retrieval(vector_store, query, k=5):
-    retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": k})
+    chroma_client = chromadb.PersistentClient(path="./hsc-llm")
+
+    # chroma_client.get_or_create_collection(
+    #     name="biology_collection", 
+    #     embedding_function=embedding_model,
+    #     metadata={"hnsw:space": "cosine"})
+
+    return Chroma(
+        client=chroma_client,
+        collection_name="biology_collection",
+        embedding_function=embedding_model,
+    )
+
+
+def add_documents_to_collection(chunks: list[Document], file_name: str):
+    collection = get_collection()
+    documents, ids = [],  []
+
+    for idx, split in enumerate(chunks):
+        documents.append(Document(page_content=split))
+        ids.append(f"{file_name}_{idx}")
+
+    collection.add_documents(documents=documents, ids=ids)
+
+
+def perform_retrieval(query: str, n = 10):
+    collection = get_collection()
+    retriever = collection.as_retriever(search_type="similarity", search_kwargs={"k": n})
     results = retriever.invoke(query)
-    print(results)
+    # print(results)
+    # print(type(results))
     return results
 
-def main():
-    #sidebar section that includes the file upload for RAG
+def get_response(context: str, prompt: str):
+    
+    llm = ChatOllama(model="llama3:latest")
+
+    prompt_template = ChatPromptTemplate([
+        ("system", system_prompt),
+        ("user", f"Context: {context}, Question: {prompt}")
+    ])
+
+    chain = prompt_template | llm | StrOutputParser()
+
+    return chain.invoke({
+        "prompt": prompt, 
+        "context": context
+    })
+
+
+def reranker(prompt: str, documents: list[str]) -> tuple[str,list[int]]:
+    document_texts = [doc.page_content for doc in documents if doc.page_content and doc.page_content.strip()]
+    
+    
+    relevant_text = ""
+    relevant_text_ids = []
+    encoder_model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+    ranks = encoder_model.rank(prompt, document_texts, top_k=3)
+    for rank in ranks:
+        relevant_text += document_texts[rank["corpus_id"]]
+        relevant_text_ids.append(rank["corpus_id"])
+
+    return relevant_text, relevant_text_ids
+
+if __name__ == "__main__":
     with st.sidebar:
         st.sidebar.title("Upload a file")
-        pdf_docs = st.file_uploader("Upload your notes", accept_multiple_files=True, key="pdf_uploader")
+        pdf_docs = st.file_uploader("Upload your notes", accept_multiple_files=False, key="pdf_uploader")
         if st.button("Upload", key="process_button"):
             with st.spinner("Processing..."):
-                add_new_docs(pdf_docs)
+                all_splits = process_document(pdf_docs)
+                add_documents_to_collection(all_splits, pdf_docs.name)
                 st.success("Done!")
 
     #conversation section
@@ -186,7 +158,6 @@ def main():
             with st.chat_message("AI"):
                 st.markdown(message.content)
 
-    #user input 
     user_query = st.chat_input("Your message")
 
     if user_query is not None and user_query != "":
@@ -194,35 +165,19 @@ def main():
 
         with st.chat_message("Human"):
             st.markdown(user_query)
-            print(app_dir)
 
-        #Testing the LLM rewriter with refined prompt query for high school teacher / student
         with st.chat_message("AI"):
-            ai_response = rewrite_query(user_query)
-            st.markdown(ai_response)
+            results = perform_retrieval(user_query)
 
-        #Testing the LLM rewriter in similarity search WITH AI answer rewriting
-        with st.chat_message("AI"):  
-            reworded_query = rewrite_query(user_query)        
-            get_keywords(reworded_query)
-            test_response = get_response(reworded_query, perform_retrieval(vector_store, reworded_query))
-            st.markdown(test_response)
+            relevant_text, relevant_text_ids = reranker(user_query, results)
+            response = get_response(context=relevant_text, prompt=user_query)
+            st.write(response)
+            
+            with st.expander("See retrieved documents"):
+                st.write(results)
 
-        #Testing the LLM rewriter in similarity search WITH AI answer rewriting AND keyword extraction retrieval
-        with st.chat_message("AI"):
-            reworded_query = rewrite_query(user_query)
-            kws = get_keywords(reworded_query)
-            print(kws[0])
-            test_response = get_response(reworded_query, perform_retrieval(vector_store, kws[0]))
-            st.markdown(test_response)
-
-        #Testing a simple similarity search WITHOUT AI answer rewriting
-        with st.chat_message("AI"):
-            ai_response = get_response(user_query, perform_retrieval(vector_store, user_query))
-            st.markdown(ai_response)
-
-        st.session_state.chat_history.append(AIMessage(ai_response))
-
-
-if __name__ == "__main__":
-    main()
+            with st.expander("See most relevant document ids"):
+                st.write(relevant_text_ids)
+                st.write(relevant_text)
+        
+        st.session_state.chat_history.append(AIMessage(response))
